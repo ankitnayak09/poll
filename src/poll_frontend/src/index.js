b83b068d3a9e5862a23c477e24c97748c9d0ca4c
Ankit Kumar Nayak <68335227+ankitnayak09@users.noreply.github.com>
2023-10-26 13:09:44 +0530

    Code Push

--- src/poll_frontend/src/index.js
+++ src/poll_frontend/src/index.js
@@ -1,19 +1,80 @@
+const pollForm = document.getElementById("radioForm");
+const resultsDiv = document.getElementById("results");
+const resetButton = document.getElementById("reset");
+
 import { poll_backend } from "../../declarations/poll_backend";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+const pollResults = {
+	Rust: 0,
+	Motoko: 0,
+	Typescript: 0,
+	Python: 0,
+};
 
-  const name = document.getElementById("name").value.toString();
+document.addEventListener(
+	"DOMContentLoaded",
+	async (e) => {
+		e.preventDefault();
 
-  button.setAttribute("disabled", true);
+		const question = await poll_backend.getQuestion();
+		document.getElementById("question").innerText = question;
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await poll_backend.greet(name);
+		const voteCounts = await poll_backend.getVotes();
+		updateLocalVoteCounts(voteCounts);
+		displayResults();
+		return false;
+	},
+	false
+);
 
-  button.removeAttribute("disabled");
+pollForm.addEventListener(
+	"submit",
+	async (e) => {
+		e.preventDefault();
 
-  document.getElementById("greeting").innerText = greeting;
+		const formData = new FormData(pollForm);
+		const checkedValue = formData.get("option");
 
-  return false;
-});
+		const updatedVoteCounts = await poll_backend.vote(checkedValue);
+		console.log("Returning from await...");
+		console.log(updatedVoteCounts);
+		updateLocalVoteCounts(updatedVoteCounts);
+		displayResults();
+		return false;
+	},
+	false
+);
+
+resetButton.addEventListener(
+	"click",
+	async (e) => {
+		e.preventDefault();
+
+		await poll_backend.resetVotes();
+		const voteCounts = await poll_backend.getVotes();
+		updateLocalVoteCounts(voteCounts);
+
+		displayResults();
+		return false;
+	},
+	false
+);
+
+function displayResults() {
+	let resultHTML = "<ul>";
+	for (let key in pollResults) {
+		resultHTML +=
+			"<li><strong>" + key + "</strong>: " + pollResults[key] + "</li>";
+	}
+	resultHTML += "</ul>";
+	resultsDiv.innerHTML = resultHTML;
+}
+
+function updateLocalVoteCounts(arrayOfVoteArrays) {
+	for (let voteArray of arrayOfVoteArrays) {
+		//Example voteArray -> ["Motoko","0"]
+		let voteOption = voteArray[0];
+		let voteCount = voteArray[1];
+		pollResults[voteOption] = voteCount;
+	}
+}
